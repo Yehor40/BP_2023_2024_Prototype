5e86e218be07f9e359984a3c01c1e74aec02d33f
Yehor Hoda <Yehor.Hoda@fnz.com>
2023-08-04 11:20:33 +0200

    Initial commit

---
+++ src/main/java/com/example/bp_2023_2024/Bp20232024Application.java
@@ -0,0 +1,13 @@
+package com.example.bp_2023_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bp20232024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bp20232024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bp_2023_2024/Bp20232024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bp_2023_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bp20232024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
