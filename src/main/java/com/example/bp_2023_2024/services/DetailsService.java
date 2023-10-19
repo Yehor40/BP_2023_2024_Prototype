eba724f2361d34f2a1456353129d9ff9b1a8f254
Yehor40 <fraugrey3@gmail.com>
2023-10-19 17:27:56 +0200

    services done

--- src/main/java/com/example/bp_2023_2024/services/DetailsService.java
+++ src/main/java/com/example/bp_2023_2024/services/DetailsService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -13,16 +15,35 @@ public class DetailsService {
 @Autowired
     private OrderDetailsRepository orderDetailsRepository;
 
-//TODO
-public int calculateWage(String id){
+    public List<OrderDetails> getAllDetails() {
+        return orderDetailsRepository.findAll();
+    }
+
+    public Optional<OrderDetails> getDetailById(String id) {
+        return orderDetailsRepository.findById(id);
+    }
+
+    public OrderDetails createDetail(OrderDetails details) {
+        return orderDetailsRepository.save(details);
+    }
+
+    public OrderDetails updateDetail(OrderDetails details) {
+        return orderDetailsRepository.save(details);
+    }
+
+    public void deleteDetail(String id) {
+        orderDetailsRepository.deleteById(id);
+    }
+
+public int calculateTotal(String id){
     OrderDetails details = orderDetailsRepository.findById(String.valueOf(id)).orElse(null);
     int res = Integer.parseInt(details.getTotal()) ;
     if (details != null) {
         return res = Integer.parseInt(details.getCharge()) * Integer.parseInt(details.getRealTime());
     }
-    // Handle the case where the entity with the given ID is not found
-    else
-    return 0;
+    else {
+        return 0;
+    }
 }
 }
 
--- src/main/java/com/example/bp_2023_2024/services/ProjectService.java
+++ src/main/java/com/example/bp_2023_2024/services/ProjectService.java
@@ -1,14 +1,34 @@
 package com.example.bp_2023_2024.services;
 
-import com.example.bp_2023_2024.repositories.OrderDetailsRepository;
+import com.example.bp_2023_2024.models.OrderProject;
+import com.example.bp_2023_2024.repositories.OrderProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
     @Autowired
-    private OrderDetailsRepository orderDetailsRepository;
-    //TODO
+    private OrderProjectRepository orderProjectRepository;
+    public List<OrderProject> getAllProjects() {
+        return orderProjectRepository.findAll();
+    }
 
+    public Optional<OrderProject> getProjectById(String id) {
 
+        return orderProjectRepository.findById(id);
+    }
+
+    public OrderProject createProject(OrderProject project) {
+        return orderProjectRepository.save(project);
+    }
+
+    public OrderProject saveProject(OrderProject project) {
+        return orderProjectRepository.save(project);
+    }
+    public void deleteProject(String id) {
+        orderProjectRepository.deleteById(id);
+    }
 }
